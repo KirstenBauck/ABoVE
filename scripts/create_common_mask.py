7ebfe86fea7b1ff024187718543c49ab7198642c
Kirsten H Bauck <khb47@rain.nauhpc>
2024-12-06 11:11:23 -0700

    Update documentation on scripts

--- scripts/create_common_mask.py
+++ scripts/create_common_mask.py
@@ -5,8 +5,24 @@ import numpy as np
 import geopandas as gpd
 import argparse
 
-# Function to read and resample raster
 def read_and_resample(file_path, transform, width, height):
+    """
+    Reads a raster file, resamples it using bilinear resampling
+    to a specified resolution, and returns the resampled data
+
+    Args:
+        file_path (str): The file path to the input raster file
+        transform (Affine): The addine transformation matric for the target resolution
+        width (int): The width of the target resampled raster in pixels
+        height (int): The height of the target resampled raster in pixels
+
+    Returns:
+        tuple: A tuple containg:
+            - resampled_data (numpy.ndarray): A 3D array containg the resampled raster
+              data with diemsions (bands, height, width)
+            - crs (CRS): the coordinate reference system of the input raster
+            - nodata (float or None): The nodata value of the raster
+    """
     with rasterio.open(file_path) as src:
         # Create an empty array for the resampled data
         resampled_data = np.empty((src.count, height, width), dtype=src.dtypes[0])
@@ -25,6 +41,16 @@ def read_and_resample(file_path, transform, width, height):
         return resampled_data, src.crs, src.nodata
 
 def calculate_common_bounds_overlap(tif_files):
+    """
+    Calculates the common bounding box overlap amoung multiple raster files
+
+    Args:
+        tif_files (list of str): A list of file paths to the input raster files
+    
+    Returns:
+        common_bounds (rasterio.coords.BoundingBox): A bounding box representing the 
+        overlaping extent of all input rasters
+    """
     common_bounds = None
     for file_path in tif_files:
         with rasterio.open(file_path) as src:
@@ -40,14 +66,27 @@ def calculate_common_bounds_overlap(tif_files):
             )
     return common_bounds
 
-def calculate_alaska_bounds():
-    canadaAlaska = "/projects/arctic/share/ABoVE_Biomass/OtherSpatialDatasets/CanadaAlaska_Boundaries.shp"
-    shapes = gpd.read_file(canadaAlaska)
-    bounds = shapes.iloc[-1]['geometry'].bounds
-    return bounds
-
 def create_na_mask(type):
+    """
+    Creates and saves a common NA mask for a specified region (Canada or Alaska) based on
+    overlaping extents of specified raster files
 
+    Args:
+        type (str): The region for which to create the NA mask.
+                    Options are "Canada" or "Alaska"
+    
+    File Paths:
+        - "Canada":
+            - `/projects/arctic/share/ABoVE_Biomass/Duncanson2023/Duncanson2023_102001.tif`
+            - `/projects/arctic/share/ABoVE_Biomass/Guindon2023/Guindon2023_102001.tif`
+            - `/projects/arctic/share/ABoVE_Biomass/Matasci2018/matasci_102001.tif`
+            - `/projects/arctic/share/ABoVE_Biomass/Soto-Navarro2020/Soto2020_102001.tif`
+            - `/projects/arctic/share/ABoVE_Biomass/SpawnGibbs2020/SpawnGibbs2020_mask_102001.tif`
+        - "Alaska":
+            - `/projects/arctic/share/ABoVE_Biomass/Duncanson2023/Duncanson2023_102001.tif`
+            - `/projects/arctic/share/ABoVE_Biomass/Soto-Navarro2020/Soto2020_102001.tif`
+            - `/projects/arctic/share/ABoVE_Biomass/SpawnGibbs2020/SpawnGibbs2020_mask_102001.tif`
+    """
     if type == "Canada":
         # Paths not included:
             # Kraatz: Way too small
@@ -60,15 +99,13 @@ def create_na_mask(type):
                         "/projects/arctic/share/ABoVE_Biomass/Soto-Navarro2020/Soto2020_102001.tif",
                         "/projects/arctic/share/ABoVE_Biomass/SpawnGibbs2020/SpawnGibbs2020_mask_102001.tif"]
         # Calculate common bound based on where they all overlap
-        target_shape = calculate_common_bounds_overlap(file_paths)
-        
     elif type == "Alaska":
         # These are the file paths we should use for the Alaska region
         file_paths = ["/projects/arctic/share/ABoVE_Biomass/Duncanson2023/Duncanson2023_102001.tif", 
                         "/projects/arctic/share/ABoVE_Biomass/Soto-Navarro2020/Soto2020_102001.tif",
                       "/projects/arctic/share/ABoVE_Biomass/SpawnGibbs2020/SpawnGibbs2020_mask_102001.tif"]
-        #target_shape = calculate_alaska_bounds()
-        target_shape = calculate_common_bounds_overlap(file_paths)
+    
+    target_shape = calculate_common_bounds_overlap(file_paths)
 
     # Set desired resolution and inirialize common NA array
     resolution = 300 # Use corsest resolution (exluding Xu)
--- scripts/zonal_stats.py
+++ scripts/zonal_stats.py
@@ -1,4 +1,3 @@
-# Import libraries
 import argparse
 import os
 from multiprocessing import Pool
@@ -10,6 +9,29 @@ from shapely.geometry import shape
 from shapely.geometry import box
 
 def process_zone(zone_row, src, file_type, coverage_ratio):
+    """
+    Processes a geographic zone to extract raster statistics based on coverage
+    within the zone's geometry
+
+    Args:
+        zone_row (Geoseries): A row from a geoDataFrame representing a geographic zone
+                              Must include a `geometry` column and column for zone ID
+        src (str): File path to the input raster file
+        file_type (str): The type of geographic zone dataset
+                            - `CanadaAlaska`: Uses the `postal` column for zone ID
+                            - `EPA2`: Uses the `NA_L2KEY` column for zone identification
+        coverage_ratio (float): The minimum faction of the zone's area that must be covered
+                                by valid raster data for the zone to be included in results
+    
+    Returns:
+        tuple: A tuple containg:
+            - zone_name (str): Name of zone
+            - mean (float or None): Mean of raster values within zone
+            - median (float or None): Median of raster values within zone
+            - sum (float or None): the sum of raster values within the zone
+            - std (float or None): The standard deviation of the raster values within the zone
+    """
+    # Get the geometry and zone name
     geometry = zone_row['geometry']
     if file_type == 'CanadaAlaska':
         zone_name = zone_row['postal']
@@ -18,15 +40,17 @@ def process_zone(zone_row, src, file_type, coverage_ratio):
 
     # Mask raster by geometry
     with rasterio.open(src) as file:
-        # Check for intersection first
         file_bounds = box(*file.bounds)
+        # Check for intersection with the zone shape
         if not file_bounds.intersects(box(*geometry.bounds)):
             print(f"Shape {zone_name} does not intersect raster, skipping.")
             return (zone_name, None, None, None, None)
         try:
+            # Get a mask of the zone shape area from the original raster
             out_image, out_transform = mask(file, [geometry], crop=True)
             no_data_value = file.nodata
             pixel_area = file.res[0] * file.res[1]
+        # There was an error in processing
         except ValueError as e:
             print(f"Error processing {zone_name}: {e}")
             return (zone_name, None, None, None, None)
@@ -42,8 +66,9 @@ def process_zone(zone_row, src, file_type, coverage_ratio):
     geometry_area = geometry.area
     actual_cover = valid_area / geometry_area
 
-    # Calculate statistics
+    # Check if the raster actually covers the zone well
     if actual_cover >= float(coverage_ratio):
+        # Calculate statistics
         mean = np.nanmean(valid_data)
         median = np.nanmedian(valid_data)
         sum = np.nansum(valid_data)
@@ -53,7 +78,20 @@ def process_zone(zone_row, src, file_type, coverage_ratio):
     return (zone_name, None, None, None, None)
 
 def calculate_zonal_stats_parallel(raster_file, shapefile, output_file, file_type, coverage_ratio):
-    print(f"Looking at dataset: {raster_file}")
+    """
+    Calculates zonal statistics for geographic zones in parallel and writes the results to a file.
+
+    Args:
+        raster_file (str): Path to the input raster file.
+        shapefile (str): Path to the shapefile containing the geographic zones.
+        output_file (str): Path to the output file where the zonal statistics will be saved.
+        file_type (str): The type of geographic zones in the shapefile.
+                            - "CanadaAlaska": Uses the 'postal' column for zone identification.
+                            - "EPA2": Uses the 'NA_L2KEY' column for zone identification.
+        coverage_ratio (float): The minimum fraction of a zone's area that must be covered by valid raster 
+                                data for it to be included in the results.
+    """
+    print(f"Looking at dataset: {raster_file} with file type {file_type}")
     shapes = gpd.read_file(shapefile)
     
     # Use multiprocessing to process zones in parallel
@@ -81,11 +119,10 @@ if __name__ == "__main__":
     elif args.script_type == 'CanadaAlaska':
         shapefile = "/projects/arctic/share/ABoVE_Biomass/OtherSpatialDatasets/CanadaAlaska_Boundaries_102001.shp"
     
-    coverage_ratio_percent = int(args.coverage_ratio * 100)
-    
     # Create output file name
     directory = os.path.dirname(args.infile)
     folder_name = os.path.basename(directory)
+    coverage_ratio_percent = int(args.coverage_ratio * 100)
     output_file = f"zonal_stats_{args.script_type}_{folder_name}_{coverage_ratio_percent}.txt"
 
     # Run parallel zonal stats and write to file
