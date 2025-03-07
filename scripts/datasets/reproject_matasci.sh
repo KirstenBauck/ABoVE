bfc2ec349e17ee31cc43a75c6666ba2065dbc344
Kirsten H Bauck <khb47@rain.nauhpc>
2025-03-07 14:25:05 -0700

    Update how datasets were retrieved

--- scripts/datasets/reproject_matasci.sh
+++ scripts/datasets/reproject_matasci.sh
@@ -11,3 +11,6 @@ module load gdal/3.7.2
 
 # Reproject
 gdalwarp -t_srs ESRI:102001 -co COMPRESS=LZW -co BIGTIFF=YES -co TILED=YES /projects/arctic/share/ABoVE_Biomass/Matasci2018/matasci_4326.tif /projects/arctic/share/ABoVE_Biomass/Matasci2018/matasci_102001.tif
+
+# Make into a bigtif for better memory
+gdal_translate -of GTiff -co BIGTIFF=YES /projects/arctic/share/ABoVE_Biomass/Matasci2018/matasci_102001.tif /projects/arctic/share/ABoVE_Biomass/Matasci2018/matasci_102001_bigtiff.tif
